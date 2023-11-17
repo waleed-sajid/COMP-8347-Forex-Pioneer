5cc573aac368f7f72a994bcb6bc7a3f10795db53
Shahzada Muhammad Hamza Khan <khan7x@uwindsor.ca>
2023-11-16 20:28:19 -0500

    Implemented the logout functionality by defining a "CustomizeLogoutPage" function that logout the user and redirects to the login page. Added corresponding URL mapping in the project "urls.py" to handle the logout action.

--- ForexPioneer/urls.py
+++ ForexPioneer/urls.py
@@ -44,6 +44,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.SignupPage, name='signup'),
     path('home/', views.HomePage, name='home'),
-    path('login/', views.LoginPage, name='login')
-
+    path('login/', views.LoginPage, name='login'),
+    path('logout/', views.LogoutPage, name='logout'),
 ]
