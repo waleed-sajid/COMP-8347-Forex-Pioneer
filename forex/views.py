0ad0f1963f01552b9aa90c4a1b890a2e42b45cd6
Muhammad Waleed Sajid <sajid.waleed19@gmail.com>
2023-11-16 19:59:57 -0500

    implement the signup functionality including html template and views.py method and created home template

---
+++ forex/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='login')
+def HomePage(request):
+    return render(request, 'home.html')
+
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        if pass1 != pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('login')
+
+    return render(request, 'signup.html')
+
+
