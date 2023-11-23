effcadd48cf6201bf1c7f2e4095bdd9a493ea551
Muhammad Waleed Sajid <sajid.waleed19@gmail.com>
2023-11-22 20:54:19 -0500

    run migration and updated tables

--- forex/admin.py
+++ forex/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Order
+from .models import UserProfile, Order, PasswordResetRequest
 
-# Register your models here.
-from .models import UserProfile
 
 admin.site.register(UserProfile)
-
 admin.site.register(Order)
+admin.site.register(PasswordResetRequest)
